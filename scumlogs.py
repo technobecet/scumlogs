dd21aef19ac320b8d645205f326c1dd577c08211
Behçet Atalay <technobecet@gmail.com>
2020-03-26 01:45:45 +0300

    updated

--- scumlogs.py
+++ scumlogs.py
@@ -9,7 +9,7 @@ import asyncio
 import aiohttp
 
 from bs4 import BeautifulSoup
-from aiocfscrape import CloudflareScraper
+import cfscrape
 from configparser import RawConfigParser
 from datetime import datetime
 
@@ -89,19 +89,18 @@ async def read_logins(syncTarget):
         loc = 'com'
     else:
         loc = 'us'
-    URL_LOGIN = 'https://id2.g-portal.com/login?redirect=https://www.g-portal.{}/en/gportalid/login?'.format(
-        configini['loc'])
-    URL_LOGS = 'https://www.g-portal.{}/en/scum/logs/{}'.format(configini['loc'], configini['serverid'])
+    URL_LOGIN = 'https://id2.g-portal.com/login?redirect=https://www.g-portal.{0}/auth/login?redirectAfterLogin=https://www.g-portal.{0}/es/'.format(configini['loc'])
+    URL_LOGS = 'https://www.g-portal.{0}/server/scum/{1}/logs'.format(configini['loc'], configini['serverid'])
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'}
 
-    async with CloudflareScraper() as session:
+    with cfscrape.create_scraper() as session:
         try:
             log('connecting g-portal...')
             payload = {'_method': 'POST', 'login': configini['user'], 'password': configini['password'],
                        'rememberme': 1}
-            async with session.post(URL_LOGIN, data=payload) as raw_response:
-                response = await raw_response.text()
-            async with session.get(URL_LOGS) as raw_response:
-                response = await raw_response.text()
+            raw_response = session.post(URL_LOGIN, headers=headers, data=payload)
+            raw_response = session.get(URL_LOGS, headers=headers)
+            response = raw_response.text
             html = BeautifulSoup(response, 'html.parser')
             select = html.find('div', {'class': 'wrapper logs'})
             logList = select['data-logs']
@@ -117,8 +116,8 @@ async def read_logins(syncTarget):
                         if id < configini[type + '_file']:
                             continue
                     payload = {'_method': 'POST', 'load': 'true', 'ExtConfig[config]': getid}
-                    async with session.post(URL_LOGS, data=payload) as raw_response:
-                        response = await raw_response.text()
+                    raw_response = session.post(URL_LOGS, headers=headers, data=payload)
+                    response = raw_response.text
                     content = json.loads(response)
                     lines = content["ExtConfig"]["content"].splitlines()
                     filename = configini['folder'] + id
@@ -138,8 +137,8 @@ async def read_logins(syncTarget):
                                 #log('admin table')
                                 await sendToServer(line, 'admin')
                             if table_1 == 'chat':
-                                #log('chat table')
-                                await sendToServer(line, 'chat')
+                                log('chat table')
+                                #await sendToServer(line, 'chat')
                             if table_1 == 'kill':
                                 #log('kill table')
                                 await sendToServer(line, 'kill')
@@ -147,8 +146,8 @@ async def read_logins(syncTarget):
                                 #log('login table')
                                 await sendToServer(line, 'login')
                             if table_1 == 'violations':
-                                #log('violations table')
-                                await sendToServer(line, 'violations')
+                                log('violations table')
+                                #await sendToServer(line, 'violations')
                             writing = True
                     if writing:
                         if found:
@@ -166,7 +165,6 @@ async def read_logins(syncTarget):
             print(e)
             log('error connecting, check connectivity and scumlogs.ini')
             help()
-        await session.close()
 
 
 if __name__ == '__main__':
